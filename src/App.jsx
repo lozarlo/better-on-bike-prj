4d8e53ba224fbcb2c3aeff2b10b329536456f198
Lorenzo Zarlenga <>
2024-05-14 11:57:09 +0200

    Added Routes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./app/pages/home/home";
-import About from "./app/pages/about/about";
-// import Location from "./app/pages/location/location";
-import Layout from "./app/pages/layout/layout";
-import Gallery from "./app/pages/gallery/gallery";
-import Error from "./app/pages/error/error";
+import Routes from "./Routes";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      id: "root",
-      path: "/",
-      element: <Layout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "/about", element: <About /> },
-        // { path: "/location", element: <Location /> },
-        { path: "/gallery", element: <Gallery /> },
-        { path: "/*", element: <Error /> },
-      ],
-    },
-  ]);
-
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ src/Routes.jsx
@@ -0,0 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./app/pages/home/home";
+import About from "./app/pages/about/about";
+import Layout from "./app/pages/layout/layout";
+import Gallery from "./app/pages/gallery/gallery";
+import Error from "./app/pages/error/error";
+
+function Routes() {
+  const router = createBrowserRouter([
+    {
+      id: "root",
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/about", element: <About /> },
+        // { path: "/location", element: <Location /> },
+        { path: "/gallery", element: <Gallery /> },
+        { path: "/*", element: <Error /> },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+export default Routes;
